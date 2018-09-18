c983b482a86129a9877f5d029338faffd34881b9
Sandra Guevara <software.rita@correo.udistrital.edu.co>
2018-09-18 14:30:13 -0500

    Adding Promises

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,54 @@
 console.log('Hola Mundo!');
+
+const noCambia = "Leonidas";
+let cambia = "@LeonidasEsteban";
+
+function cambiarNombre(nuevoNombre) {
+    cambia = nuevoNombre;
+}
+
+const getUser = new Promise(function(todoBien, todoMal) {
+    //llamar a un api
+    setTimeout(function() {
+        todoMal('Se acabó el tiempo');
+        // todoBien('Todo está bien en la vida');
+    }, 3000);
+});
+
+const getUserAll = new Promise(function(todoBien, todoMal) {
+    //llamar a un api
+    setTimeout(function() {
+        // todoMal('Se acabó el tiempo');
+        todoBien('Todo está bien en la vida');
+    }, 5000);
+});
+
+// getUser
+//     .then(function() {
+//         console.log('Todo está bien en la vida');
+//     })
+//     .catch(function(message) {
+//         console.log(message);
+//     })
+
+// Promise.all([
+//     getUser,
+//     getUserAll
+// ])
+// .then(function(message) {
+//     console.log(message);
+// })
+// .catch(function(message) {
+//     console.log(message);
+// })
+
+Promise.race([
+    getUser,
+    getUserAll
+])
+.then(function(message) {
+    console.log(message);
+})
+.catch(function(message) {
+    console.log(message);
+})
