33554ee535a8b83e9aea62019212b3e4ca597d41
Nathaly <natharb09@gmail.com>
2018-09-18 20:41:19 -0500

    Adding Asyncronous Functions and Selectors

--- src/js/home.js
+++ src/js/home.js
@@ -73,4 +73,33 @@ fetch('https://randomuser.me/api/sgsngisng')
     })
     .catch(function() {
         console.log('algo falló')
-    })
+    });
+
+(async function load() {
+    async function getData(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+
+    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action');
+    const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama');
+    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation');
+    console.log(actionList, dramaList, animationList);
+
+    const $actionContainer = document.querySelector('#action');
+    const $dramaContainer = document.getElementById('#drama');
+    const $animationContainer = document.getElementById('#animation');
+    const $featuringContainer = document.getElementById('#featuring');
+    const $form = document.getElementById('#form');
+    const $home = document.getElementById('#home');
+
+    const $modal = document.getElementById('modal');
+    const $overlay = document.getElementById('overlay');
+    const $hideModal = document.getElementById('hide-modal');
+
+    const modalTitle = $modal.querySelector('h1');
+    const modalImage = $modal.querySelector('img');
+    const modalDescription = $modal.querySelector('p');
+
+})();
